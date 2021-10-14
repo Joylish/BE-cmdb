d3c927958f62db767690266bf797e9876040e5e9
Joylish <yeonzzu2@gmail.com>
2021-10-14 17:16:22 +0900

    env : initialize spring boot project

---
+++ src/main/java/com/sktelecom/core/cmdb/BeCmdbApplication.java
@@ -0,0 +1,13 @@
+package com.sktelecom.core.cmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeCmdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeCmdbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sktelecom/core/cmdb/BeCmdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sktelecom.core.cmdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeCmdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
